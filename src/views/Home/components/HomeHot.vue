bb94f28b46fc3e52190158b48d6f601f8320c358
历一鸣 <2358514943@qq.com>
2024-07-06 17:19:22 +0800

    第四次提交

--- src/views/Home/components/HomeHot.vue
+++ src/views/Home/components/HomeHot.vue
@@ -14,7 +14,7 @@ onMounted(()=>getHotList());
 <template>
 <HomePanel title="人气推荐" sub-title="人气爆款，不容错过">
   <ul class="goods-list">
-    <li v-for="item in hotList":key="item.id">
+    <li v-for="item in hotList" :key="item.id">
      <RouterLink to="/">
        <img :src="item.picture" alt=""/>
        <p class="name">{{item.title}}</p>
